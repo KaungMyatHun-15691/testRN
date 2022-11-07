d9a4765133bdc72aa699c45842b2d9d96a42272c
Kaung Myat Hun <kaungmyathun7@gmail.com>
2022-11-07 16:34:07 +0630

    Creating: UI

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello</Text>      
+      <Text>Next Test Word</Text>
+      <Text style={styles.text}>Hello again</Text>
+      <Button title="Click me"></Button>
     </View>
   );
 }
@@ -17,4 +18,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    margin: 10,
+  }
 });
